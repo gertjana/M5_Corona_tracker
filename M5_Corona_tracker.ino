3dec7aaea13b707d88a2ed6faa2f15579990bba9
g.assies <gertjan.assies@gmail.com>
2020-04-01 21:41:05 +0200

    Added Trend graph, and some  refactoring

--- M5_Corona_tracker.ino
+++ M5_Corona_tracker.ino
@@ -3,10 +3,14 @@
 #include <HTTPClient.h>
 #include "ArduinoJson.h"
 #include "secrets.h"
+#include <Preferences.h>
 
 #define NR_COUNTRIES 5
-const int REFRESH_RATE = 60; //seconds
+#define TREND_LENGTH 8
+const int REFRESH_RATE = 10; //seconds
 const String countries[NR_COUNTRIES]  = {"gb","us","it", "es", "cn"};
+
+Preferences prefs;
 
 int cnt_http = 0;
 bool refresh_http = true;
@@ -17,12 +21,12 @@ String scope = "World";
 String path = "/totals";
 int country_index = 0;
 StaticJsonDocument<300> doc;
-
-
+int trend_int[TREND_LENGTH];
+int TREND_FACTOR = 8;
 
 void setup(void) {
   Serial.begin(115200);
-  while(!Serial);    // time to get serial running
+  while(!Serial);    // wait until we have serial running
   Serial.println("Corona Tracker");
     
   M5.begin(true, false, true); //lcd + serial enabled, sd card disabled
@@ -36,6 +40,34 @@ void setup(void) {
   Serial.println("");
   Serial.print("WiFi connected with IP address: ");
   Serial.println(WiFi.localIP());
+
+}
+
+
+void loop() {
+  M5.update();
+
+  read_buttons();
+  
+  cnt_http += 1;
+  
+  if (cnt_http > 10*REFRESH_RATE) {
+    cnt_http = 0;
+    refresh_http = true;
+  }
+
+  if (refresh_http) {
+    refresh_http = false;
+
+    get_http();
+  
+    store_trend(confirmed);
+  
+    update_display();
+
+  }
+    
+  delay(100); //milliseconds
 }
 
 void update_display() {
@@ -44,6 +76,7 @@ void update_display() {
 
   M5.Lcd.setTextColor(TFT_WHITE,TFT_BLACK);
   M5.Lcd.setTextFont(4);
+  scope.toUpperCase();
   M5.Lcd.println(String("Corona Tracker - ") + scope);
 
   M5.Lcd.setTextFont(2);
@@ -66,6 +99,35 @@ void update_display() {
   M5.Lcd.setTextColor(TFT_RED,TFT_BLACK);
   M5.Lcd.setTextFont(7);
   M5.Lcd.println(deaths);
+
+  Serial.println(scope.equalsIgnoreCase("World"));
+  Serial.println(scope.equalsIgnoreCase(String("World")));
+  if (scope.equalsIgnoreCase("World")) {
+    for (int i=0;i<TREND_LENGTH;i++) {
+      int val = trend_int[i]/TREND_FACTOR;
+      if (val > 150) {val = 150;}
+      int x = i*10+230;
+      int w = 8;
+      int y = 220-val;
+      int h = val;
+      M5.Lcd.drawRect(x, y, w, h, TFT_ORANGE);
+      M5.Lcd.fillRect(x, y, w, h, TFT_ORANGE);
+    }
+    M5.Lcd.setTextColor(TFT_WHITE,TFT_BLACK);
+    M5.Lcd.drawLine(230,70,230,220, TFT_WHITE);
+    M5.Lcd.drawLine(230,220,310,220, TFT_WHITE);
+
+    M5.Lcd.setTextFont(2);
+
+    M5.Lcd.setCursor(230, 222);
+    M5.Lcd.println("new cases");
+    
+    M5.Lcd.setCursor(205,85);
+    M5.Lcd.println(" 1k");
+
+    M5.Lcd.setCursor(205,210);
+    M5.Lcd.println(" 0");
+  }
 }
 
 void parse(String payload) {
@@ -76,9 +138,7 @@ void parse(String payload) {
   deaths = doc[0]["deaths"];
 }
 
-void loop() {
-  M5.update();
-  
+void read_buttons() {
   if (M5.BtnA.read()) {
     scope = "World";
     path = "/totals";
@@ -96,19 +156,12 @@ void loop() {
     }
     refresh_http = true;
   }
+}
 
-  cnt_http += 1;
-  
-  if (cnt_http > 10*REFRESH_RATE) {
-    cnt_http = 0;
-    refresh_http = true;
-  }
-
-  if (refresh_http) {
-    refresh_http = false;
-  
-    HTTPClient http;
+void get_http() {
+ HTTPClient http;
     http.begin(String("https://") + host + path);
+    http.addHeader("Accept", "application/json");
     http.addHeader("X-rapidapi-host", host);
     http.addHeader("X-rapidapi-key", apikey);
   
@@ -120,11 +173,79 @@ void loop() {
     } else {
       Serial.println("Error on HTTP request");
     }
-    
-    update_display();
+    http.end();
+}
 
-    http.end(); //Free the resources
-  }
+void store_trend(int value) {
+  Serial.println(scope);
+
+  char schema[sizeof(scope)];
+  scope.toCharArray(schema, sizeof(scope));
+  
+  char trend[sizeof(schema)+6];
+  strcpy(trend, schema);
+  strcat(trend, "_trend");
+
+  prefs.begin("trend");
+  int prev_value = prefs.getInt(schema, 0);
     
-  delay(100); //milliseconds
+  if (prev_value == value) {
+    Serial.println("No change");
+
+    char trend_buffer[TREND_LENGTH*2];
+    prefs.getBytes(trend, trend_buffer, TREND_LENGTH*2);
+    trend_to_integer(trend_buffer, trend_int);
+    
+    prefs.end();
+    //return;
+  } 
+  
+  prefs.putInt(schema, value);
+
+  int diff = value - prev_value;
+
+  char trend_buffer[TREND_LENGTH*2];
+  prefs.getBytes(trend, trend_buffer, TREND_LENGTH*2);
+  
+  for (int i=0;i<(TREND_LENGTH*2-2); i++) {
+    trend_buffer[i] = trend_buffer[i+2];
+  }
+  trend_buffer[TREND_LENGTH*2-2] = (diff >> 8) & 0xFF;
+  trend_buffer[TREND_LENGTH*2-1] = diff & 0xFF;
+  
+  trend_to_integer(trend_buffer, trend_int);
+
+  prefs.putBytes(trend, &trend_buffer, TREND_LENGTH*2);
+
+  prefs.end();
+}
+
+void trend_to_integer(char t[], int trend[]) {
+  print_array("Trend", t, TREND_LENGTH*2);  
+  for (int i=0;i<TREND_LENGTH;i++) {
+    trend[i] = (t[i*2] << 8) + t[i*2+1];  
+  }
+  print_array_int("Trend", trend, TREND_LENGTH*2);  
+}
+
+void print_array(char name[], char data[], int len) {
+  Serial.print(name);
+  Serial.print(": ");
+  for (int i=0; i<len; i++) {
+    if (data[i]<0x10) {Serial.print("0");}
+    Serial.print(data[i],HEX);
+    Serial.print(" ");
+  }
+  Serial.println("");
+}
+
+void print_array_int(char name[], int a[], int len) {
+  int l = len/2;
+  Serial.print(name);
+  Serial.print(": ");
+  for (int i=0; i<l; i++) {
+    Serial.print(a[i]);
+    if (i != l-1) { Serial.print(", "); }
+  }
+  Serial.println("");  
 }
